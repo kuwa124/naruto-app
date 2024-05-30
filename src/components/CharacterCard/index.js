5b1b2eee448ab17e40478096c494d67c2f4c87a8
kuwa124 <c0mwgave1407@gmail.com>
2024-05-30 12:02:37 +0900

    ♻️ リファクタリング

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -8,6 +8,16 @@ const CharacterCard = ({ character }) => {
   return (
     // カードコンポーネントのdiv要素
     <div className="card">
+      {/* カードのコンテンツ部分 */}
+      <div className="card-content">
+        {/* キャラクター名を表示する */}
+        <h3 className="card-title">キャラクター名：{character.name}</h3>
+        {/* キャラクターのデビュー作品を表示する (nullの場合は 'なし' と表示する) */}
+        <p className="card-description">
+          登場コンテンツ：
+          {character.debut?.appearsIn ?? ' なし'}
+        </p>
+      </div>
       {/* キャラクターの画像を表示する */}
       <img
         // 画像URLを設定する (nullの場合はダミー画像を使用する)
@@ -15,19 +25,11 @@ const CharacterCard = ({ character }) => {
         alt="character" // 代替テキスト
         className="card-image" // CSSクラス
       />
-      {/* カードのコンテンツ部分 */}
-      <div className="card-content">
-        {/* キャラクター名を表示する */}
-        <h3 className="card-title">{character.name}</h3>
-        {/* キャラクターのデビュー作品を表示する (nullの場合は 'なし' と表示する) */}
-        <p className="card-description">
-          {character.debut?.appearsIn ?? ' なし'}
-        </p>
-      </div>
       {/* カードのフッター部分 */}
       <div className="card-footer">
         {/* キャラクターの所属を表示する (nullの場合は 'なし' と表示する) */}
         <span className="affiliation">
+          所属：
           {character.personal?.affiliation ?? 'なし'}
         </span>
       </div>
